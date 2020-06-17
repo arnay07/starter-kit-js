30ea4512c8c3eda755618e052cb50660c549b0ba
arnaud kaderi <arnay07@gmail.com>
2020-06-17 19:41:29 +0200

    added fetch
    support getting users

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
+import {getUsers} from './api/userApi';
+
 import "./index.css";
 
-import numeral from "numeral";
+getUsers().then(result => {
+  let usersBody = "";
+  result.forEach(user => {
+    usersBody += `<tr>
+        <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+        <td>${user.id}</td>
+        <td>${user.firstName}</td>
+        <td>${user.lastName}</td>
+        <td>${user.email}</td>
+        </tr>`
+  });
 
-const courseValue = numeral(1000).format("$0,0.00");
-console.log(`I would pay ${courseValue} for this awesome course`);
+  global.document.getElementById('users').innerHTML = usersBody;
+});
--- src/index.test.js
+++ src/index.test.js
@@ -10,14 +10,14 @@ describe("First test", () => {
 });
 
 describe("index.html", () => {
-  it("should say hello", (done) => {
+  it("should say h1 that says Users", (done) => {
     const index = fs.readFileSync('./src/index.html', "utf-8");
     /* you can pass an array of js files to load into the jsdom env as the second parameter*/
     /*this is asynchronous so we add done so that expect might be run add done so that mocha
     knows that the test is asynchronous*/
     jsdom.env(index, function(err, window) {
       const h1 = window.document.getElementsByTagName('h1')[0];
-      expect(h1.innerHTML).to.equal("Hello world");
+      expect(h1.innerHTML).to.equal("Users");
       done();
       window.close();
     });
